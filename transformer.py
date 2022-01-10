315de467ffa6638d8b139ded64a9b3cbdaca72c3
Arthur Cahu <arthur.cahu@gmail.com>
2022-01-10 18:29:00 +0100

    updated license for coherence

--- transformer.py
+++ transformer.py
@@ -1,5 +1,24 @@
 # Adapted from https://github.com/ChinaYi/ASFormer
-# Copyright (c) 2021 ChinaYi (MIT license)
+# MIT License
+# Copyright (c) 2021 ChinaYi
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import torch
 import torch.nn as nn
@@ -136,7 +155,6 @@ class AttLayer(nn.Module):
         _, c3, _ = v.size()
         
         
-        #assert m_batchsize == 1  # currently, we only accept input with batch size 1
         # padding zeros for the last segment
         nb = L // self.bl 
         if L % self.bl != 0:
